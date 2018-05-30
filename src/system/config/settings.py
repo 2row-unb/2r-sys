980fcd3ea5f2bdcb25cda9bfb92ba3c9ed13e9b7
Luan Guimarães <luang@protonmail.ch>
2018-05-30 14:57:00 -0300

    Refactor settings to use Gabby

--- src/system/config/settings.py
+++ src/system/config/settings.py
@@ -2,45 +2,6 @@
 Global configurations of 2RSystem
 """
 
-from collections import namedtuple
-
-Topic = namedtuple('Topic', ['topic', 'fmt'])
-Module = namedtuple('Module', ['name', 'input', 'output'])
-
-
-def enum(**named_args):
-    return type('MetaConfig', (), named_args)
-
-
-Config = enum(
-    general={
-        'URL': "localhost",
-        'PORT': 1883,
-    },
-
-    controller=Module('controller', None, None),
-
-    processor=Module(
-        'processor',
-        Topic('2rs/processor/input', 'f'*23),
-        Topic('2rs/processor/output', 'f'*23)
-    ),
-
-    transmitter=Module(
-        'transmitter',
-        Topic('2rs/transmitter/input', 'i'),
-        Topic('2rs/transmitter/output', 'i')
-    ),
-
-    receiver=Module(
-        'receiver',
-        Topic('2rs/receiver/input', None),
-        Topic('2rs/receiver/output', 'f'*23)
-    ),
-
-    kernel=Module(
-        'kernel',
-        Topic('2rs/transmitter/output', 'i'),
-        Topic('2rs/receiver/input', None),
-    )
-)
+MOSQUITTO_URL = 'localhost'
+MOSQUITTO_PORT = 1883
+MOSQUITTO_KEEPALIVE = 60
--- src/system/controller.py
+++ src/system/controller.py
@@ -2,51 +2,19 @@
 2RS Controller
 """
 import logging
+import gabby
 
-from .config.settings import Config
-from .rxtx import Rx, Tx
-from .message import Writer
-from .decorators import on_message, decode_message
 from .helpers import make_runner
 
 
-class Controller(Rx, Writer):
-    def __init__(self):
-        input_topics = (
-            Config.receiver.output.topic,
-            Config.processor.output.topic,
-        )
-        super().__init__(input_topics)
-
-        output_topics = (
-            (Config.transmitter.name, Config.transmitter.input.topic),
-            (Config.processor.name, Config.processor.input.topic),
-        )
-        self.tx = Tx(dict(output_topics))
-
-    @on_message
-    def _on_message(self, client, userdata, message):
-        logging.info("[Controller] Message received")
-        self.act(message)
-
-    @decode_message(Config.receiver.name)
-    def act(self, message):
-        """
-        Format mqtt messages
-        """
-        for msg in self.unzip_message(message):
-            self.tx.publish(msg)
-            logging.info(f'[Controller] Publishing to {msg.to}')
-
-    def unzip_message(self, message):
-        """
-        Divide message data by significant parts
-        """
-        # [FIXME] implemente split logical
-        return (
-            self.write_message([1], to=Config.transmitter.name),
-            self.write_message(message.data, to=Config.processor.name),
-        )
+class Controller(gabby.Gabby):
+    """
+    Class to controll all system operations and dispatch messages to
+    the responsible modules
+    """
+    def transform(self, message):
+        logging.info(f'Transforming data {message.data}')
+        return [gabby.Message(message.data, self.output_topics)]
 
 
 run = make_runner(Controller)
