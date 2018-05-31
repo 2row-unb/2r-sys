328bb9d94403b7556115d57bf820f40fd91835ed
Luan Guimarães <luang@protonmail.ch>
2018-05-30 21:07:47 -0300

    Fix transmitter output topic

--- src/system/topics.py
+++ src/system/topics.py
@@ -8,7 +8,7 @@ _topics = {
    "kernel_receiver":           gabby.Topic('2rs/receiver/input', 'f'*23),
    "receiver_controller":       gabby.Topic('2rs/receiver/output', 'f'*23),
    "controller_transmitter":    gabby.Topic('2rs/trasmitter/input', 'f'*23),
-   "trasmitter_kernel":         gabby.Topic('2rs/transmitter/output', 'f'*23),
+   "transmitter_kernel":         gabby.Topic('2rs/transmitter/output', 'f'*23),
    "controller_processor":      gabby.Topic('2rs/processor/input', 'f'*23),
    "processor_controller":      gabby.Topic('2rs/processor/output', 'f'*23),
 }
