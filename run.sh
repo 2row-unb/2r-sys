0c538b3f65eb1de07f5420b07f13bae5d1b9eab3
Luan Guimarães <luang@protonmail.ch>
2018-06-11 02:56:09 -0300

    Fix tmux windows position

--- run.sh
+++ run.sh
@@ -7,11 +7,11 @@ SCREEN_C="transmitter"
 SCREEN_D="processor"
 
 tmux new-session -d
-tmux split-window -d -t 0 -h
-tmux split-window -d -t 0 -h
 tmux split-window -d -t 0 -v
-tmux split-window -d -t 2 -v
-tmux split-window -d -t 4 -v
+tmux split-window -d -t 0 -v
+tmux split-window -d -t 0 -h
+tmux split-window -d -t 2 -h
+tmux split-window -d -t 4 -h
 tmux send-keys -t 0 "inv run $(echo $@) -i ${SCREEN_A}" enter
 tmux send-keys -t 1 "inv run $(echo $@) -i ${SCREEN_B}" enter
 tmux send-keys -t 2 "inv run $(echo $@) -i ${SCREEN_C}" enter
