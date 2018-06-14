1d35b971785775ea29b96443461fd228e2e8500e
Luan Guimarães <luang@protonmail.ch>
2018-06-14 17:33:43 -0300

    Add command to run MQTT-SN broker

--- run.sh
+++ run.sh
@@ -10,6 +10,7 @@ SCREEN_E="kernelcontrol"
 tmux new-session -d
 tmux split-window -d -t 0 -v
 tmux split-window -d -t 0 -v
+tmux split-window -d -t 2 -v
 tmux split-window -d -t 0 -h
 tmux split-window -d -t 2 -h
 tmux split-window -d -t 4 -h
@@ -17,6 +18,7 @@ tmux send-keys -t 0 "inv run $(echo $@) -i ${SCREEN_A}" enter
 tmux send-keys -t 1 "inv run $(echo $@) -i ${SCREEN_B}" enter
 tmux send-keys -t 2 "inv run $(echo $@) -i ${SCREEN_C}" enter
 tmux send-keys -t 3 "inv run $(echo $@) -i ${SCREEN_D}" enter
-tmux send-keys -t 5 "inv run $(echo $@) -i ${SCREEN_E}" enter
-tmux send-keys -t 4 "inv flask" enter 
+tmux send-keys -t 4 "inv run $(echo $@) -i ${SCREEN_E}" enter
+tmux send-keys -t 5 "inv flask" enter 
+tmux send-keys -t 6 "./mqttsn/rsmb/rsmb/src/broker_mqtts ./mqttsn/rsmb.conf" enter
 tmux attach
