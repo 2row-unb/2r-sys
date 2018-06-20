8cbee88dfc205122b4a098d30766157e4db440ff
Lucas Mattioli <lucas.mattioli@live.com>
2018-06-20 03:13:40 -0300

    Add clear method for calibrator and fix semantic errors.

--- src/system/calibrator.py
+++ src/system/calibrator.py
@@ -1,7 +1,9 @@
 import time
+import logging
+from random import *
 
-DESIRED_SAMPLES = 1000
-ACCEPTED_DIFFERENCE = 10.0
+DESIRED_SAMPLES = 100
+ACCEPTED_DIFFERENCE = 10000.0
 TIME_TO_START = 10
 
 class Calibrator():
@@ -11,12 +13,15 @@ class Calibrator():
         self._samples = 0
 
     def add_sample(self, sample_data, current_time):
+        if self.ready():
+            return True
+
         time_elapsed = current_time - self._start_time
-        if time_elapsed < TIME_TO_START or self.ready():
-            return
+        if time_elapsed < TIME_TO_START:
+            return False
 
         if self._axes_sums[0] == None:
-            self._axes_sums = sample_data
+            self._axes_sums = [*sample_data]
             self._samples = 1
         else:
             accepted_sample = True
@@ -32,8 +37,10 @@ class Calibrator():
                     self._axes_sums[i] += sample_data[i]
                 self._samples += 1
             else:
-                self._axes_sums = sample_data
+                self._axes_sums = [*sample_data]
                 self._samples = 1
+
+        return self.ready()
 
     def ready(self):
         return self._samples >= DESIRED_SAMPLES
@@ -43,3 +50,8 @@ class Calibrator():
         for i in range(0, len(self._axes_sums)):
             results.append(self._axes_sums[i] / self._samples)
         return results
+
+    def clear(self):
+        self._start_time = time.time()
+        self._axes_sums = [None, None, None]
+        self._samples = 0
