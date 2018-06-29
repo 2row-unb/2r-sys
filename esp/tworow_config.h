b39b53abec2b44bf444a0eea60893f63bc1db34c
Lucas Mattioli <lucas.mattioli@live.com>
2018-06-29 12:54:55 -0300

    Update IP.

--- esp/tworow_config.h
+++ esp/tworow_config.h
@@ -9,7 +9,7 @@ const char* wifi_ssid = "2ROW-PRIVATE";
 const char* wifi_password = "123chaim4";
 
 // Custom variables for TWOROW SYSTEM
-char udp_host[] = "192.168.1.101";
+char udp_host[] = "192.168.1.100";
 unsigned int udp_port = 1885;
 MQTTSNClient client = {"suit", udp_host, udp_port};
 unsigned char ek_topic[] = "eq";
